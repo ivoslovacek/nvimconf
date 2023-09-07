47f4171a01a8e19fbb94f344bbda75aff1763b32
Ivo Slováček <ivoslovacek@gmail.com>
2023-09-07 19:14:23 +0200

    added lsp-zero

--- lua/ivoslovacek/init.lua
+++ lua/ivoslovacek/init.lua
@@ -9,7 +9,6 @@ vim.opt.softtabstop = 4
 
 vim.wo.number = true
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -26,48 +25,10 @@ vim.opt.rtp:prepend(lazypath)
 require("ivoslovacek/remap")
 require("lazy").setup("ivoslovacek.plugins")
 require("nvim-web-devicons")
+require("ivoslovacek.configs.lualine")
+require("ivoslovacek.configs.lspzero")
+-- load colorscheme
 require("catppuccin").setup({
 	flavour = "frappe"
 })
-require('lualine').setup {
-  options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
-  },
-  sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
-    lualine_y = {},
-    lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
-}
-
 vim.cmd.colorscheme("catppuccin-frappe")
