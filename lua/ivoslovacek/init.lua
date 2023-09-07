b04490e96c3eaaf60f5432dcf9369b0c06c02827
Ivo Slováček <ivoslovacek@gmail.com>
2023-09-07 19:54:49 +0200

    formatting

--- lua/ivoslovacek/init.lua
+++ lua/ivoslovacek/init.lua
@@ -2,14 +2,14 @@ require("ivoslovacek.sets")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -20,6 +20,6 @@ require("ivoslovacek.configs.lualine")
 require("ivoslovacek.configs.lspzero")
 -- load colorscheme
 require("catppuccin").setup({
-	flavour = "frappe"
+    flavour = "frappe"
 })
 vim.cmd.colorscheme("catppuccin-frappe")
--- lua/ivoslovacek/remap.lua
+++ lua/ivoslovacek/remap.lua
@@ -1,22 +1,22 @@
 vim.g.mapleader = " "
 
 -- buffer switching
-vim.keymap.set("n", "<Tab>", vim.cmd.bnext, {desc = "Switch to next buffer"})
-vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious, {desc = "Switch to previous buffer"})
+vim.keymap.set("n", "<Tab>", vim.cmd.bnext, { desc = "Switch to next buffer" })
+vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious, { desc = "Switch to previous buffer" })
 
 -- moving text in visual mode
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selected text up a line"})
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selected text down a line"})
-vim.keymap.set("x", "<leader>p", "\"_dp", {desc = "Keep pasted text in register"})
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected text up a line" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selected text down a line" })
+vim.keymap.set("x", "<leader>p", "\"_dp", { desc = "Keep pasted text in register" })
 
 -- copying text
-vim.keymap.set("n", "<leader>y", "\"+y", {desc = "Yank text to system clipboard"})
-vim.keymap.set("n", "<leader>Y", "\"+Y", {desc = "Yank line to system clipboard"})
-vim.keymap.set("v", "<leader>y", "\"+y", {desc = "Yank text to system clipboard"})
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "Yank text to system clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank line to system clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Yank text to system clipboard" })
 
 -- nvimtree
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus, {desc = "Open and focus NvimTree"})
-vim.keymap.set("n", "<leader>pc", vim.cmd.NvimTreeClose, {desc = "Close NvimTree"})
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus, { desc = "Open and focus NvimTree" })
+vim.keymap.set("n", "<leader>pc", vim.cmd.NvimTreeClose, { desc = "Close NvimTree" })
 
 -- telescope
 vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, {})
