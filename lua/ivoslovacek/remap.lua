f1e503f7b0f30c1ad15d51e957bb6fb0f99ea9bd
Ivo Slováček <ivoslovacek@gmail.com>
2023-09-07 13:37:26 +0200

    added more remaps

--- lua/ivoslovacek/remap.lua
+++ lua/ivoslovacek/remap.lua
@@ -4,9 +4,19 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<Tab>", vim.cmd.bnext, {desc = "Switch to next buffer"})
 vim.keymap.set("n", "<S-Tab>", vim.cmd.bprevious, {desc = "Switch to previous buffer"})
 
+-- moving text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {desc = "Move selected text up a line"})
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {desc = "Move selected text down a line"})
+vim.keymap.set("x", "<leader>p", "\"_dp", {desc = "Keep pasted text in register"})
+
+-- copying text
+vim.keymap.set("n", "<leader>y", "\"+y", {desc = "Yank text to system clipboard"})
+vim.keymap.set("n", "<leader>Y", "\"+Y", {desc = "Yank line to system clipboard"})
+vim.keymap.set("v", "<leader>y", "\"+y", {desc = "Yank text to system clipboard"})
+
 -- nvimtree
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus)
-vim.keymap.set("n", "<leader>pc", vim.cmd.NvimTreeClose)
+vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus, {desc = "Open and focus NvimTree"})
+vim.keymap.set("n", "<leader>pc", vim.cmd.NvimTreeClose, {desc = "Close NvimTree"})
 
 -- telescope
 vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_files() end, {})
