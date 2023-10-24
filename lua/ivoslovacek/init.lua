2ad770ebc819bd8473d909e248fea821c4fcbff2
Ivo Slováček <ivoslovacek@gmail.com>
2023-10-24 11:14:43 +0200

    Enabled transparent background

--- lua/ivoslovacek/init.lua
+++ lua/ivoslovacek/init.lua
@@ -25,3 +25,6 @@ require("catppuccin").setup({
     flavour = "frappe"
 })
 vim.cmd.colorscheme("catppuccin-frappe")
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
